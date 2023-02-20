a52502cc49a930af0e04229390da3528f31246ef
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-02-20 19:28:48 +0100

    V0

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'zaritus3@hotmail.com'   
   def welcome_email(user)               
     @user = user                                                     
-    @url  = 'https://maltaevents.herokuapp.com/'                                 
+    @url  = 'http://monsite.fr/login'                               
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,18 +4,6 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-# ActionMailer::Base.smtp_settings = {
-#   user_name: ENV['SENDGRID_LOGIN'],
-#   password: ENV['SENDGRID_PWD'],
-#   domain: 'maltaevents.herokuapp.com',
-#   address: 'smtp.sendgrid.net',
-#   port: 587,
-#   authentication: :plain,
-#   enable_starttls_auto: true
-# }
-
-
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
